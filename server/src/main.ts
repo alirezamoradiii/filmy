0b75a0c0bbd1bc5c601499c59c1b156ad6c45f72
Alireza Moradi <me.alirezamoradi@gmail.com>
2026-08-16 14:38:44 +0330

    feat: setup swagger, global prefix and strict validation pipe
    Co-authored-by: emma-wilson-dev <alirezamoradi.official7@gmail.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import helmet from 'helmet';
 import compression from 'compression';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import swagger from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.use(helmet());
   app.use(compression());
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  SwaggerModule.setup('/api/docs', app, swagger(app));
 
   await app.listen(process.env.PORT ?? 3000);
 }
