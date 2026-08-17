32bc98d6cb5ec52df0d83283e5f78f3ac17da6f5
Alireza Moradi <me.alirezamoradi@gmail.com>
2026-08-17 07:54:20 +0330

    feat: setup postgres database connection using typeorm
    Co-authored-by: emma-wilson-dev <alirezamoradi.official7@gmail.com>

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import appConfig from '../configs/app.config';
+import pgConfig from '../configs/pg.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const ENV = process.env.NODE_ENV;
 
@@ -11,8 +13,22 @@ const ENV = process.env.NODE_ENV;
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ENV ? `.env.${ENV}.local` : '.env',
-      load: [appConfig],
+      load: [appConfig, pgConfig],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (service: ConfigService) => ({
+        type: 'postgres',
+        host: service.get<string>('pgConfig.host'),
+        port: service.get<number>('pgConfig.port'),
+        username: service.get<string>('pgConfig.username'),
+        password: service.get<string>('pgConfig.password'),
+        database: service.get<string>('pgConfig.database'),
+        autoLoadEntities: true,
+        synchronize: ENV === 'development' ? true : false
+      })
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
