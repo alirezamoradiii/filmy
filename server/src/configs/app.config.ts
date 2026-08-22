55d47509b96fbae4f5ab0caf927ed02bc49383c7
Alireza Moradi <me.alirezamoradi@gmail.com>
2026-08-22 12:14:40 +0330

    feat(mail): generate mail module and configure MailerModule with EjsAdapter
    Co-authored-by: emma-wilson-dev <alirezamoradi.official7@gmail.com>

--- server/src/configs/app.config.ts
+++ server/src/configs/app.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('appConfig', () => ({
   port: process.env.PORT,
   mail_host: process.env.MAIL_HOST,
-  mail_port: process.env.MAIL_PORT,
+  mail_port: parseInt(process.env.MAIL_PORT || '2525', 10),
   mail_userName: process.env.MAIL_USER,
   mail_password: process.env.MAIL_PASS,
 }));
