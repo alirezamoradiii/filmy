1ece076737e2ad7a0237dd38c6ce204fe2e1442c
Alireza Moradi <me.alirezamoradi@gmail.com>
2026-08-22 11:57:22 +0330

    chore(mail): setup mailer config, dependencies, EJS engine
    Co-authored-by: emma-wilson-dev <alirezamoradi.official7@gmail.com>

--- server/src/configs/app.config.ts
+++ server/src/configs/app.config.ts
@@ -2,4 +2,8 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('appConfig', () => ({
   port: process.env.PORT,
+  mail_host: process.env.MAIL_HOST,
+  mail_port: process.env.MAIL_PORT,
+  mail_userName: process.env.MAIL_USER,
+  mail_password: process.env.MAIL_PASS,
 }));
